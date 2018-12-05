9f0a3b3c5a94d9500d5b4a586a57cda16f65e0b0
yoshi3315 <yoshiki.chiba@one-star.jp>
2018-12-05 02:20:24 +0000

    minitest-reporters gemをインストールし設定

--- test/fizz_buzz_test.rb
+++ test/fizz_buzz_test.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 require 'minitest/autorun'
+require "minitest/reporters"
+Minitest::Reporters.use!
+
 require './lib/fizz_buzz'
 
 class FizzBuzzTest < Minitest::Test
