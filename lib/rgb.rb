02372bf41fa666ebc21c25fc6eaca3fec3c8ec46
yoshi3315 <yoshiki.chiba@one-star.jp>
2018-12-05 04:36:20 +0000

    メソッドをさらにDRYにリファクタリング

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 def to_hex(r, g, b)
-  hex = '#'
-  [r, g, b].each do |color|
-    hex += color.to_s(16).rjust(2, '0')
+  [r, g, b].inject('#') do |hex, color|
+    hex + color.to_s(16).rjust(2, '0')
   end
-  hex
 end
