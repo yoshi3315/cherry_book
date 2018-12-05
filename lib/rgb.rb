03c1a45d685b3c0c52aeb117cadf3669f24c3c13
yoshi3315 <yoshiki.chiba@one-star.jp>
2018-12-05 04:23:45 +0000

    メソッドがないとエラーになるのでメソッドを定義

---
+++ lib/rgb.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+def to_hex(r, g, b)
+  '#000000'
+end
--- test/rgb_test.rb
+++ test/rgb_test.rb
@@ -4,6 +4,8 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 Minitest::Reporters.use!
 
+require './lib/rgb'
+
 class RgbTest < Minitest::Test
   def test_to_hex
     assert_equal '#000000', to_hex(0, 0, 0)
