a12cb447b7c8173b4998f8882a8aa1585f4cda2f
yoshi3315 <yoshiki.chiba@one-star.jp>
2018-12-06 06:52:16 +0000

    固定値をメソッドの外で定数定義

--- lib/convert_length.rb
+++ lib/convert_length.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
+UNITS = { m: 1.0, ft: 3.28, in: 39.37}
+
 def convert_length(length, from: :m, to: :m)
-  units = { m: 1.0, ft: 3.28, in: 39.37}
-  (length / units[from] * units[to]).round(2)
+  (length / UNITS[from] * UNITS[to]).round(2)
 end
