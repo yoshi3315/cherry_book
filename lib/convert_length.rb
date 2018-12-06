e7f3dc36b085f3d9259e7702aa0fb9a1e7a90420
yoshi3315 <yoshiki.chiba@one-star.jp>
2018-12-06 06:08:00 +0000

    メソッドがないとエラーになるのでメソッドを簡単に定義

---
+++ lib/convert_length.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+def convert_length(length, unit_from, unit_to)
+  39.37
+end
--- test/convert_length_test.rb
+++ test/convert_length_test.rb
@@ -4,6 +4,8 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 Minitest::Reporters.use!
 
+require './lib/convert_length'
+
 class ConvertLengthTest < Minitest::Test
   def test_convert_length
     assert_equal 39.37, convert_length(1, 'm', 'in')
