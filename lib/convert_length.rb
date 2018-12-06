f94f737cec66c33dd1e5533a2ff93ecc2d0b6f72
yoshi3315 <yoshiki.chiba@one-star.jp>
2018-12-06 07:09:24 +0000

    Rubocopのエラーを手動修正

--- lib/convert_length.rb
+++ lib/convert_length.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-UNITS = { m: 1.0, ft: 3.28, in: 39.37}
+UNITS.freeze = { m: 1.0, ft: 3.28, in: 39.37 }
 
 def convert_length(length, from: :m, to: :m)
   (length / UNITS[from] * UNITS[to]).round(2)
