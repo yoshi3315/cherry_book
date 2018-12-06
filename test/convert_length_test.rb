8b01488a10bfd3788a41e78eabb07c1d566034e4
yoshi3315 <yoshiki.chiba@one-star.jp>
2018-12-06 06:11:15 +0000

    テストに検証コードを追加

--- test/convert_length_test.rb
+++ test/convert_length_test.rb
@@ -9,5 +9,6 @@ require './lib/convert_length'
 class ConvertLengthTest < Minitest::Test
   def test_convert_length
     assert_equal 39.37, convert_length(1, 'm', 'in')
+    assert_equal 0.38, convert_length(15, 'in', 'm')
   end
 end
