73479acbcbf668a60b6c45d04b1c1b02fe6a94d7
yoshi3315 <yoshiki.chiba@one-star.jp>
2018-12-07 05:20:43 +0000

    1つ目のシナリオの検証コードをテストに記載

--- test/gate_test.rb
+++ test/gate_test.rb
@@ -8,6 +8,10 @@ require './lib/gate'
 
 class GateTest < Minitest::Test
   def test_gate
-    assert Gate.new
+    umeda = Gate.new(:umeda)
+    juso = Gate.new(:juso)
+    ticket = Ticket.new(150)
+    umeda.enter(ticket)
+    assert juso.exit(ticket)
   end
 end
