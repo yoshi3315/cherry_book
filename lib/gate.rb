ffaa952fb8c67c558acc9f21cbdcddc3a3f3eb34
yoshi3315 <yoshiki.chiba@one-star.jp>
2018-12-07 07:26:25 +0000

    rubocopエラーを手動修正、定数からインデックス番号を取得する際にシンボルで引数渡す必要があるので修正

--- lib/gate.rb
+++ lib/gate.rb
@@ -1,25 +1,25 @@
 # frozen_string_literal: true
 
 class Gate
-  STATIONS = [:umeda, :juso, :mikuni]
-  FARES = [150, 190]
-  
+  STATIONS = %i[umeda juso mikuni].freeze
+  FARES = [150, 190].freeze
+
   def initialize(name)
     @name = name
   end
-  
+
   def enter(ticket)
     ticket.stamp(@name)
   end
-  
+
   def exit(ticket)
     calculated_fare = calc_fare(ticket)
     calculated_fare <= ticket.fare
   end
-  
+
   def calc_fare(ticket)
-    from = STATIONS.index(ticket.stamped_at)
-    to = STATIONS.index(@name)
+    from = STATIONS.index(ticket.stamped_at.to_sym)
+    to = STATIONS.index(@name.to_sym)
     distance = (to - from).abs
     FARES[distance - 1]
   end
--- test/gate_test.rb
+++ test/gate_test.rb
@@ -13,25 +13,25 @@ class GateTest < Minitest::Test
     @juso = Gate.new(:juso)
     @mikuni = Gate.new(:mikuni)
   end
-  
+
   def test_umeda_to_juso
     ticket = Ticket.new(150)
     @umeda.enter(ticket)
     assert @juso.exit(ticket)
   end
-  
+
   def test_umeda_to_mikuni_when_fare_is_not_enough
     ticket = Ticket.new(150)
     @umeda.enter(ticket)
     refute @mikuni.exit(ticket)
   end
-  
+
   def test_umeda_to_mikuni_when_fare_is_enough
     ticket = Ticket.new(190)
     @umeda.enter(ticket)
     assert @mikuni.exit(ticket)
   end
-  
+
   def test_juso_to_mikuni
     ticket = Ticket.new(150)
     @juso.enter(ticket)
