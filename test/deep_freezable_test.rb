9e0be9c718b988f921efc877ede353fb728cc8a1
yoshi3315 <yoshiki.chiba@one-star.jp>
2018-12-14 05:04:41 +0000

    配列を凍結させられているかのテストを記載

--- test/deep_freezable_test.rb
+++ test/deep_freezable_test.rb
@@ -4,10 +4,13 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 Minitest::Reporters.use!
 
-require './lib/deep_freezable'
+require './lib/bank'
+require './lib/team'
 
 class DeepFreezableTest < Minitest::Test
-  def test_deep_freeze
-    assert DeepFreezable
+  def test_deep_freeze_to_array
+    assert_equal ['japan', 'US', 'India'], Team::COUNTRIES
+    assert Team::COUNTRIES.frozen?
+    assert Team::COUNTRIES.all? { |country| country.frozen? }
   end
 end
