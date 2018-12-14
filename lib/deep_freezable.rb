51781f138650e6bcc23b18ff78c8c26dee50238d
yoshi3315 <yoshiki.chiba@one-star.jp>
2018-12-14 05:46:53 +0000

    hashを凍結させるメソッドを記載

--- lib/deep_freezable.rb
+++ lib/deep_freezable.rb
@@ -2,8 +2,16 @@
 
 module DeepFreezable
   def deep_freeze(array_or_hash)
-    array_or_hash.each do |element|
-      element.freeze
+    case array_or_hash
+    when Array
+      array_or_hash.each do |element|
+        element.freeze
+      end
+    when Hash
+      array_or_hash.each do |key, value|
+        key.freeze
+        value.freeze
+      end
     end
     array_or_hash.freeze
   end
