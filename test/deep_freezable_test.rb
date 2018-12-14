5a09fd69de54009946201957443900c190abf5ce
yoshi3315 <yoshiki.chiba@one-star.jp>
2018-12-14 05:29:43 +0000

    ハッシュを凍結させられているかのテストを記載

--- test/deep_freezable_test.rb
+++ test/deep_freezable_test.rb
@@ -13,4 +13,13 @@ class DeepFreezableTest < Minitest::Test
     assert Team::COUNTRIES.frozen?
     assert Team::COUNTRIES.all? { |country| country.frozen? }
   end
+
+  def test_deep_freeze_to_hash
+    assert_equal(
+      { 'japan' => 'yen', 'US' => 'dollar', 'India' => 'rupee' },
+      Bank::CURRENCIES
+    )
+    assert Bank::CURRENCIES.frozen?
+    assert Bank::CURRENCIES.all? { |key, value| key.frozen? && value.frozen? }
+  end
 end
