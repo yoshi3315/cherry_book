e8363c12059280c2ec46045e890dea07989f390e
yoshi3315 <yoshiki.chiba@one-star.jp>
2018-12-17 13:01:02 +0000

    不正な正規表現パターンが入力された際に例外処理を実行し再入力させるように修正

--- lib/regexp_checker.rb
+++ lib/regexp_checker.rb
@@ -2,10 +2,15 @@
 
 print 'Text?: '
 text = gets.chomp
-print 'Pattern?: '
-pattern = gets.chomp
+begin
+  print 'Pattern?: '
+  pattern = gets.chomp
+  regexp = Regexp.new(pattern)
+rescue RegexpError => e
+  puts "Invalid pattern: #{e.message}"
+  retry
+end
 
-regexp = Regexp.new(pattern)
 matches = text.scan(regexp)
 if matches.size > 0
   puts "Matched: #{matches.join(', ')}"
